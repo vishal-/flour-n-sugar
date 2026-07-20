ec098c0f389cb86f2b2d1c42781c6a753b108b4f
Vishal <ims.vishal@gmail.com>
2026-07-20 22:41:14 +0530

    Initial commit

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBVg09BrauH7CZWeGd6EGHg0yUVQSxUZWA",
+  authDomain: "flour-n-sugar.firebaseapp.com",
+  projectId: "flour-n-sugar",
+  storageBucket: "flour-n-sugar.firebasestorage.app",
+  messagingSenderId: "755976058353",
+  appId: "1:755976058353:web:6dfbc50afbc32cef6a4050",
+  measurementId: "G-7NDE6C5E4R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
